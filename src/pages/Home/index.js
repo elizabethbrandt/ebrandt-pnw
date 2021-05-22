dd790fe66434c36b35f55b69da6fe9d4c603849e
Elizabeth Brandt <ebrandt8@uw.edu>
2021-05-22 11:06:16 -0700

    added floral logo and created columns within jumbotron

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,25 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Jumbotron } from "react-bootstrap";
+import { Col, Container, Jumbotron, Row } from "react-bootstrap";
+import Logo from "../../images/EBLogo.png"
 
 export default function Home() {
     return (
         <Jumbotron>
-            <h1>Elizabeth Brandt</h1>
-            <h3>Web Developer</h3>
-            <Button>Learn about me</Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <img 
+                            src={Logo}
+                            alt="Floral logo design displaying Elizabeth Brandt, Full Stack Web Developer"
+                        />
+                    </Col>
+                    <Col>
+                        <h1>Brand statement here</h1>
+                        <Button>Learn about me</Button>
+                    </Col>
+                </Row>
+            </Container>
         </Jumbotron>
     )
 }
