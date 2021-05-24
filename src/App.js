fc586532f0e8ad2bdb41c5a9c6ed58bd1026e79f
Elizabeth Brandt <ebrandt8@uw.edu>
2021-05-23 23:21:09 -0700

    edited exact path

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/Nav';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import About from './pages/About';
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Switch>
-        <Route exactpath="/" component={Home}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
       </Switch>
       <Footer />
     </Router>
