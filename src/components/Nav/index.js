fc65bbade16c957ebe405bb4cc8d65e8dd23bbf6
Elizabeth Brandt <ebrandt8@uw.edu>
2021-05-23 23:22:41 -0700

    added missing about path on href

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -15,7 +15,7 @@ export default function Navigation() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/">About</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="/projects">Projects</Nav.Link>
                     <Nav.Link href="/resume">Resume</Nav.Link>
                     <Nav.Link href="/contact">Contact</Nav.Link>
