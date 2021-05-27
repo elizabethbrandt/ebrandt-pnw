2b5887f926db10c83060e8811d8b6a3d3dcecd1a
Elizabeth Brandt <ebrandt8@uw.edu>
2021-05-26 23:27:21 -0700

    edited form names and types

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -10,45 +10,40 @@ export default function Contact() {
                 <Form>
                     <h2>Contact Me</h2>
                     <Form.Row>
+
+                        <Form.Group controlId="formGridAddress1">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control placeholder="Jane Smith" />
+                        </Form.Group>
+
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="number" placeholder="555-555-5555" />
                         </Form.Group>
                     </Form.Row>
 
-                    <Form.Group controlId="formGridAddress1">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="1234 Main St" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formGridAddress2">
-                        <Form.Label>Address 2</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
-                    </Form.Group>
-
                     <Form.Row>
-                        <Form.Group as={Col} controlId="formGridCity">
-                            <Form.Label>City</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>State</Form.Label>
+                            <Form.Label>Interest in talking to me</Form.Label>
                             <Form.Control as="select" defaultValue="Choose...">
                                 <option>Choose...</option>
-                                <option>...</option>
+                                <option>Collaboration</option>
+                                <option>Employment</option>
+                                <option>Help</option>
                             </Form.Control>
                         </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>Zip</Form.Label>
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>Additional Comments</Form.Label>
                             <Form.Control />
                         </Form.Group>
+
                     </Form.Row>
 
                     <Form.Group id="formGridCheckbox">
