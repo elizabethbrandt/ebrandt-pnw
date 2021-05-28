f96e6936ffbf97b4a9f3257d04780d0c3382fc5a
Elizabeth Brandt <ebrandt8@uw.edu>
2021-05-27 20:17:53 -0700

    added projectinfo object

---
+++ src/components/ProjectCard/Projects.js
@@ -0,0 +1,36 @@
+const ProjectInfo = [
+    {
+        title: "Coin Créatif",
+        description: "A user-friendly and mobile forward e-commerce application for artists and creative types. This platform helps sellers showcase their handmade items and post them for sale.",
+        technologies: "ReactJS, JSX, Material-UI, MongoDB, Mongoose, NodeJS, Express, Multer",
+        deployed: "https://coincreatif.herokuapp.com/",
+        github: "https://github.com/elizabethbrandt/coincreatif",
+        screenshot: "../../images/coin-creatif.png"
+    },
+    {
+        title: "Book Club",
+        description: "Create an account to keep track of the books you've read, are reading, or want to read. Search to add any book to your bookshelf to show off what you've",
+        technologies: "JavaScript, Express, Handlebars, Passport, Sequelize, API",
+        deployed: "https://postnet-project-2.herokuapp.com/",
+        github: "https://github.com/ToMakPo/UWCB-Project-2",
+        screenshot: "../../images/book-club.png"
+    },
+    {
+        title: "Kitchen Helper",
+        description: "Ever gone to make dinner, seen what's in your fridge, and been at a loss for what to make? Use this application to find recipes based on ingredients you already have in your home.",
+        technologies: "HTML, CSS, JavaScript, API",
+        deployed: "https://jkaganovsky.github.io/Kitchen-Helper/",
+        github: "https://github.com/jkaganovsky/Kitchen-Helper",
+        screenshot: "../../images/kitchen-helper.png"
+    },
+    {
+        title: "Note Taker",
+        description: "All your reminders in one place with this note taking application. You can come back and see or edit any old notes.",
+        technologies: "HTML, CSS, JavaScript, Express",
+        deployed: "https://eb-note-taker.herokuapp.com/",
+        github: "https://github.com/elizabethbrandt/Note-Taker",
+        screenshot: "../../images/note-taker.png"
+    }
+]
+
+export default ProjectInfo;
