18c75ca71a73ecce8c0c7009cbd23ab101c09b8f
Elizabeth Brandt <ebrandt8@uw.edu>
2021-06-05 23:48:15 -0700

    added bootstrap icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import { Container, Jumbotron, Row } from "react-bootstrap";
+import { EnvelopeFill } from "react-bootstrap-icons";
 
 export default function Footer() {
     return (
@@ -9,7 +10,8 @@ export default function Footer() {
                     <div className="jumbotron col mr-2 text-center green" id="contact-me">
                         <h1 className="display-5 font-weight-normal">Contact Me</h1>
                         <p className="lead pt-2"><a href="tel:541-908-1121"><i className="fas fa-mobile-alt"></i> 541-908-1121</a></p>
-                        <p className="lead"><a href="mailto:elizabeth.brandt824@gmail.com"><i className="far fa-envelope"></i>
+                        <EnvelopeFill />
+                        <p className="lead"><a href="mailto:elizabeth.brandt824@gmail.com">
                         elizabeth.brandt824@gmail.com</a></p>
                         <p className="lead"><a href="https://github.com/elizabethbrandt" target="_blank" className="lead"><i
                             className="fab fa-github"></i>GitHub Profile</a></p>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,14 +11,14 @@ export default function Home() {
                     <Col>
                         <img 
                             src={Logo}
-                            height="700"
-                            length="700"
+                            height="600"
+                            length="600"
                             alt="Floral logo design displaying Elizabeth Brandt, Full Stack Web Developer"
                         />
                     </Col>
                     <Col>
-                        <h4>Full Stack Web Developer utilizing experience in human resources, project management, and healthcare to create polished, user-friendly, and organized websites and software platforms. Earned a certificate in Full Stack Web Development from the University of Washington. Adaptable problem-solver who is passionate about developing applications that focus on clarity, ease, and efficiency for all while still providing a powerful tool for their needs. Strengths in planning, project management, process improvement, and leadership.
-                        </h4>
+                        <h6>Full Stack Web Developer utilizing experience in human resources, project management, and healthcare to create polished, user-friendly, and organized websites and software platforms. Earned a certificate in Full Stack Web Development from the University of Washington. Adaptable problem-solver who is passionate about developing applications that focus on clarity, ease, and efficiency for all while still providing a powerful tool for their needs. Strengths in planning, project management, process improvement, and leadership.
+                        </h6>
                         <Button href="/about">Learn more about me</Button>
                     </Col>
                 </Row>
